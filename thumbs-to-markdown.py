a4892eee7ef97a1b2819dffb63437bfe048b26ed
Isaiah Odhner <isaiahodhner@gmail.com>
2024-09-10 15:40:31 -0400

    Handle Windows-style paths

--- thumbs-to-markdown.py
+++ thumbs-to-markdown.py
@@ -20,16 +20,27 @@ def find_scenes():
 				scenes.append(Scene(scene_name, scene_path, thumb_path))
 	return scenes
 
+def path_to_relative_url(path: str) -> str:
+	# Replace backslashes with forward slashes (for Windows paths)
+	path = path.replace("\\", "/")
+	# URL-encode the path (this ignores forward slashes by default)
+	return urllib.parse.quote(path, safe="/")
+
 def generate_markdown(scenes):
 	markdown = ''
+
+	# TODO: sort so it's consistent across operating systems / file systems
+	# sort by name
+	# TODO: sort numerically for version numbers in scene names
+	# scenes.sort(key=lambda scene: scene.name)
 
 	# sort by whether there is a thumbnail
 	scenes.sort(key=lambda scene: scene.thumb_path is None)
 
 	for scene_name, scene_path, thumb_path in scenes:
-		scene_url_path = urllib.parse.quote(scene_path)
+		scene_url_path = path_to_relative_url(scene_path)
 		if thumb_path:
-			thumb_url_path = urllib.parse.quote(thumb_path)
+			thumb_url_path = path_to_relative_url(thumb_path)
 			markdown += f'[![{scene_name}]({thumb_url_path})]({scene_url_path})\n'
 		else:
 			markdown += f'- [{scene_name}]({scene_url_path})\n'
